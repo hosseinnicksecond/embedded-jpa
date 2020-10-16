42034595d52010ac23a679f95f07960bcec5ed7a
hossein nick <hossinnick62@gmail.com>
2020-10-16 20:10:11 +0330

    create maven project and add .gitignore and clean install

---
+++ src/main/java/home/train/EmbeddedAnnotationApplication.java
@@ -0,0 +1,13 @@
+package home.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedAnnotationApplication.class, args);
+	}
+
+}
